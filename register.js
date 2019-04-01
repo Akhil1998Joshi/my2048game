c00dcc54c49815add2791b933a331ca2eae9500c
Akhil Joshi <akhil.joshi1997@gmail.com>
2019-04-02 00:34:29 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,44 @@
+var deferredPrompt;
+var enableNotificationsButtons = document.querySelectorAll('.enable-notifications');
+
+if (!window.Promise) {
+    window.Promise = Promise;
+}
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js').then(function () {
+        console.log('serviceWorer is registered');
+    });
+}
+window.addEventListener('beforeinstallprompt', function (event) {
+    event.prompt();
+    // Wait for the user to respond to the prompt
+    event.userChoice
+        .then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+
+        });
+});
+
+
+function askForNotificationPermission() {
+    Notification.requestPermission(function (result) {
+        console.log('User Choice', result);
+        if (result !== 'granted') {
+            console.log('No notification permission granted!');
+        } else {
+            displayConfirmNotification();
+        }
+    });
+}
+
+if ('Notification' in window) {
+    for (var i = 0; i < enableNotificationsButtons.length; i++) {
+        enableNotificationsButtons[i].style.display = 'inline-block';
+        enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
+    }
+}
